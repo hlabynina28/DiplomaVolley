597a1b1b264bbf2c9b628e8f84b03be690097edf
V <ss002008@mail.ru>
2026-06-19 01:46:20 +0300

    download

--- beach_volleyball/settings.py
+++ beach_volleyball/settings.py
@@ -102,7 +102,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.mail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
